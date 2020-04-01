b7d0ab018f8d219b56c444f658eb9238d3537930
Moe_Nya <33063741+Moe-Nya@users.noreply.github.com>
2020-04-01 16:36:42 +0800

    Add files via upload

---
+++ BV-AV.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace UP主动态更新
+{
+    public partial class 获取av号 : Form
+    {
+        public 获取av号()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //按钮
+        private void search_Click(object sender, EventArgs e)
+        {
+            //查询文本框内是否有内容
+            if(string.IsNullOrEmpty(UUID.Text))
+            {
+                MessageBox.Show("请输入BV号", "标题", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if((UUID.Text) == "请输入UP主的ID")
+            {
+                MessageBox.Show("请输入BV号", "标题", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            String url = "https://api.bilibili.com/x/web-interface/view?bvid=[REPLACE]";
+            String Url = url.Replace("[REPLACE]", UUID.Text);
+
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
+            Request.Timeout = 20 * 1000;//请求超时。
+            Request.AllowAutoRedirect = true; //网页自动跳转。
+            Request.UserAgent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";//伪装成谷歌爬虫。
+            Request.Method = "GET"; //获取数据的方法。GET
+                                    //Request.Method = "POST";//POST
+                                    //Request.ContentType = "application/json";上传的格式JSON
+            Request.KeepAlive = true; //保持
+            HttpWebResponse Response = (HttpWebResponse)Request.GetResponse();
+            using (StreamReader sReader = new StreamReader(Response.GetResponseStream(), Encoding.UTF8))
+            {
+                String Htmlstring = sReader.ReadToEnd();
+
+                int i = Htmlstring.IndexOf("\"aid\":");
+                int j = Htmlstring.IndexOf(",\"videos\"");
+                Htmlstring = (Htmlstring.Substring(i + 6)).Substring(0, j - i - 6);
+                ht.Text = "AV号是:av" + Htmlstring;
+            }
+
+        }
+    }
+}
